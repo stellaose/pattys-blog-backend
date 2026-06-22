b3c8cfb4bb02e094da6b6b78b50ea1c85091f48d
Stella Oseyomon <admin@SF-MACBOOK-010.local>
2026-06-22 15:00:49 +0100

    feat:added logger to log errors in the signup and verify email endpoint

--- server/enums/messagesEnum.js
+++ server/enums/messagesEnum.js
@@ -1,3 +1,17 @@
 export const messagesEnum = {
+  // ! Authentication related messages
   EMAIL_REQUIRED: "Email is required",
+  EMAIL_EXISTS: "Email already exists",
+  EMAIL_NOT_VERIFIED: "Email not verified",
+  EMAIL_ALREADY_VERIFIED: "This e-mail has already been verified. Please login",
+  PASSWORD_REQUIRED: "Password is required (min 8 chars)",
+  USERNAME_REQUIRED: "Username is required",
+  USERNAME_EXISTS: "Username already exists",
+  PHONE_NUMBER_REQUIRED: "Phone Number is required",
+  VERIFICATION_CODE_REQUIRED: "Verification code is required",
+  INVALID_VERIFICATION_CODE: "Invalid or expired verification code",
+  INVALID_PASSWORD: "Invalid password",
+  INVALID_USERNAME: "Invalid username",
+  INVALID_EMAIL: "Invalid email",
+  INVALID_TOKEN: "Invalid token",
 }
